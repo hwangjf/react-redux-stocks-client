4ddae751f9276a25e2069cd3f5a5d0149248ec58
hwangjf <jeffreyhwang90@gmail.com>
2019-04-25 09:49:59 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+
+// import Provider and createStore and add Store to Provider around App
+// add in react-thunk -> docs for how to add to createStore
+
+ReactDOM.render(<App />, document.getElementById('root'));
